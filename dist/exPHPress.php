06e631bb3ef002f1dc36e62b5a37300341a92a7c
nifte <9247037+nifte@users.noreply.github.com>
2018-08-31 15:57:21 -0700

    fixed formatting

--- dist/exPHPress.php
+++ dist/exPHPress.php
@@ -1,62 +1,81 @@
 <?php
-	$static = '';
-	$route = array_slice(array_filter(explode('/', $_SERVER['REDIRECT_URL'])), count(array_filter(explode('/', $_SERVER['PHP_SELF'])))-1);
-	$valid_route = false;
+	define('APP_URL', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'] . implode('/', array_slice(explode('/', $_SERVER['PHP_SELF']), 0, -1)));
+	define('REDIRECT_URL', explode('#', explode('?', $_SERVER['REQUEST_URI'])[0])[0]);
+	define('ROUTE', array_slice(array_filter(explode('/', REDIRECT_URL)), count(array_filter(explode('/', $_SERVER['PHP_SELF']))) - 1));
 
-	if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') $protocol = 'https://';
-	else $protocol = 'http://';
-	define('HOME_URL', $protocol . $_SERVER['SERVER_NAME'] . implode('/', array_slice(explode('/', $_SERVER['PHP_SELF']), 0, -1)));
-	
 	class exPHPress {
+		function __construct() {
+			$this->app_dir = getcwd();
+			$this->error_fn = function() {
+				http_response_code(404);
+				echo 'Page not found.';
+			};
+			register_shutdown_function(function() {
+				if (!$this->valid_route) {
+					chdir($this->app_dir);
+					$res = new Response;
+					$res->static_dir = $this->static_dir;
+					call_user_func_array($this->error_fn, [[], $res]);
+				}
+			});
+		}
+		private $app_dir = '',
+			$static_dir = '',
+			$error_fn = '',
+			$valid_route = false;
 		public function get($url, $function) {
 			$this->test('GET', $url, $function);
-		}
-		public function post($url, $function) {
-			$this->test('POST', $url, $function);
 		}
 		public function put($url, $function) {
 			$this->test('PUT', $url, $function);
 		}
+		public function post($url, $function) {
+			$this->test('POST', $url, $function);
+		}
+		public function patch($url, $function) {
+			$this->test('PATCH', $url, $function);
+		}
 		public function delete($url, $function) {
 			$this->test('DELETE', $url, $function);
 		}
-		public function invalid($function) {
-			global $invalid_fn;
-			$invalid_fn = $function;
+		public function any($url, $function) {
+			$this->test($_SERVER['REQUEST_METHOD'], $url, $function);
+		}
+		public function error($function) {
+			$this->error_fn = $function;
 		}
 		private function test($method, $url, $function) {
-			global $route;
 			$url = array_filter(explode('/', trim($url, '/')));
-			if (count($route) == count($url)) {
+			if (count(ROUTE) == count($url)) {
 				$route_matches = true;
 				$req = [];
-				for ($i = 0; $i < count($route); $i++) {
-					if (preg_match('/^:/', $url[$i])) $req[trim($url[$i], ':')] = $route[$i];
-					else if ($route[$i] != $url[$i]) {
+				for ($i = 0; $i < count(ROUTE); $i++) {
+					if (preg_match('/^:/', $url[$i])) $req[trim($url[$i], ':')] = ROUTE[$i];
+					else if (ROUTE[$i] != $url[$i]) {
 						$route_matches = false;
 						break;
 					}
 				}
 				if ($_SERVER['REQUEST_METHOD'] == $method && $route_matches) {
-					global $valid_route;
-					$valid_route = true;
-					call_user_func_array($function, [$req, new Response]);
+					$this->valid_route = true;
+					$res = new Response;
+					$res->static_dir = $this->static_dir;
+					call_user_func_array($function, [$req, $res]);
 				}
 			}
 		}
 		public function static($dir) {
 			if (is_dir($dir)) {
-				global $static;
-				$static = trim($dir, '/') . '/';
+				$this->static_dir = trim($dir, '/') . '/';
 			}
 		}
 	}
 
 	class Response {
+		public $static_dir = '';
 		public function sendFile($file, $vars = null) {
-			global $static;
 			if (is_array($vars)) extract($vars);
-			include $static . $file;
+			include $this->static_dir . $file;
 		}
 		public function sendStatus($status) {
 			http_response_code($status);
@@ -75,24 +94,7 @@
 			} else if (count($args) == 2) header("$args[0]: $args[1]");
 		}
 		public function redirect($url) {
-			$protocol = $_SERVER['REQUEST_SCHEME'] . '://';
-			$location = $_SERVER['SERVER_NAME'] . implode('/', array_slice(explode('/', $_SERVER['PHP_SELF']), 0, -1));
-			$app = $protocol . $location;
-			$newLocation = $app . '/' . trim($url, '/');
-			header("Location: $newLocation");
+			$new_location = APP_URL . '/' . trim($url, '/');
+			header("Location: $new_location");
 		}
 	}
-
-	$invalid_fn = function() {
-		http_response_code(404);
-		echo 'Page not found.';
-	};
-
-	$cwd = getcwd();
-	register_shutdown_function(function() {
-		global $valid_route, $invalid_fn, $cwd;
-		if (!$valid_route) {
-			chdir($cwd);
-			call_user_func_array($invalid_fn, [[], new Response]);
-		}
-	});
